778477e02397f26804b3c64855f9266f20c49c12
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-09 14:00:47 +0600

    summary added

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("how are you");
+/**
+ * So now day's there are few options you can take from my inner hart. so first thing first
+ */
---
+++ summary.js
@@ -0,0 +1,7 @@
+/**
+ * React core concept
+ * 1. Component = similar in look different in data.
+ * 2. Container component = all component parent.
+ * 3. Divided component = for easy to navigate.
+ * 4. Stand alone component = not relation with others.
+ */
