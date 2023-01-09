517f3b8a9a8969b34d63f047daafa281d2927abd
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2023-01-09 15:21:38 +0600

    added

--- practice-app/src/components/Country/Country.js
+++ practice-app/src/components/Country/Country.js
@@ -4,7 +4,30 @@ export default function Country({ country }) {
   console.log(country);
   return (
     <div>
-      <h1>Name {country.name.common}</h1>
+      <div class="card mb-3" style={{ maxWidth: "540px" }}>
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              src={country.flags.png}
+              class="img-fluid rounded-start"
+              alt="flags"
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{country.name.common}</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
